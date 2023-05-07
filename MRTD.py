1ee49d6b63f60c7d80e29c13e81fb27b56f4614b
Dhruv Patel <dhruvpatel.r78@gmail.com>
2023-05-07 14:51:25 -0400

    added encode and decode in mrtd.py

--- MRTD.py
+++ MRTD.py
@@ -1,4 +1,9 @@
-import json
+from string import ascii_uppercase, digits
+from tkinter import END
+import pycountry
+from googletrans import Translator
+import time
+import datetime
 
 CHAR_DICT = {
     '<': 0,
@@ -40,6 +45,43 @@ CHAR_DICT = {
     'z': 35,
 }
 
+printable = digits + ascii_uppercase + "<"+";"
+
+def algorithm(string: str) -> str:
+    string = string.upper().replace("<", "0")
+    weight = [7, 3, 1]
+    summation = 0
+    for i in range(len(string)):
+        c = string[i]
+        summation += printable.index(c) * weight[i % 3]
+    summation %=10
+    return summation
+
+def vertify (string0: str) -> str:
+    for i in range(len(string0)):
+        c = string0[i]
+        if c not in printable:
+            return ("contains invalid characters")
+    string1 = string0[45:]
+    passport = string1[0:9]
+    passport_vertify_code = int(string1[9])
+    birth = string1[13:19]
+    birth_vertify_code = int(string1[19])
+    validity = string1[21:27]
+    validity_vertify_code = int(string1[27])
+    personal_code = string1[28:43]
+    personal_vertify_code = int(string1[43])
+
+    if algorithm(passport) != passport_vertify_code:
+        return("passport info error")
+    elif algorithm(birth) != birth_vertify_code:
+        return("birth date info error")
+    elif algorithm(validity) != validity_vertify_code:
+        return("validity info error")
+    elif algorithm(personal_code) != personal_vertify_code:
+        return("personal code error")
+    else:
+        return("passed")
 
 def char_to_value(char):
     # Returns numeric value for input char used in check digit algorithm
@@ -74,3 +116,42 @@ def extract_line1(line):
         "last_name": last_name,
         "given_name": given_name
     }
+    
+def encode(string0: str) -> str:
+    string1 = string0[45:]
+    passport_number = string1[0:9]
+    country_code = string1[10:13]
+    birth_date = string1[13:19]
+    sex = string1[20]
+    expiration_date = string1[21:27]
+    personal_number = string1[28:37]
+
+    d= {}
+    k={}
+    d['line2'] =  k
+    k['passport_number'] = passport_number
+    k['country_code'] = country_code
+    k['birth_date'] = birth_date 
+    k['sex'] = sex
+    k['expiration_date'] = expiration_date
+    k['personal_number'] = personal_number
+
+    return d
+
+def decode(dict):
+    dict1 = dict["line1"]
+    dict2 = dict["line2"]
+    issc = dict1["issuing_country"]
+    lastname = dict1["last_name"]
+    givenname = dict1["given_name"]
+    passport = dict2["passport_number"]
+    country = dict2["country_code"]
+    birth = dict2["birth_date"]
+    sex = dict2["sex"]
+    exd = dict2["expiration_date"]
+    pn = dict2["personal_number"]
+    line1decode = "P<" + issc + lastname + "<<" + givenname.replace(' ','<')
+    line1decoded1 = line1decode.ljust(44,'<')
+    line2decode = passport + str(algorithm(passport)) + country + birth + str(algorithm(birth)) + sex +exd + str(algorithm(exd)) + pn + "<<<<<<" + str(algorithm(pn))
+    decoded = line1decoded1 +";"+line2decode
+    return decoded
