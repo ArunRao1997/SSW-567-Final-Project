9c668b24b6b72926c99bb057a348821636b437cc
ArunRao1997 <rao.nayinenii@gmail.com>
2023-05-07 20:54:07 -0400

    Optimizing MRTD.py

--- MRTD.py
+++ MRTD.py
@@ -1,5 +1,7 @@
-"""MRTD.py"""
-from string import ascii_uppercase, digits
+"""
+Module for parsing and generating Machine Readable Travel Documents (MRTD).
+"""
+import json
 
 CHAR_DICT = {
     '<': 0,
@@ -41,68 +43,27 @@ CHAR_DICT = {
     'z': 35,
 }
 
-printable = digits + ascii_uppercase + "<"+";"
-
-def algorithm(string: str) -> str:
-    """Function for weights"""
-    string = string.upper().replace("<", "0")
-    weight = [7, 3, 1]
-    summation = 0
-    for i in range(len(string)):
-        c = string[i]
-        summation += printable.index(c) * weight[i % 3]
-    summation %=10
-    return summation
-
-def verify (string0: str) -> str:
-    """function to verify"""
-    for i in range(len(string0)):
-        c = string0[i]
-        if c not in printable:
-            return "contains invalid characters"
-    string1 = string0[45:]
-    passport = string1[0:9]
-    passport_verify_code = int(string1[9])
-    birth = string1[13:19]
-    birth_verify_code = int(string1[19])
-    validity = string1[21:27]
-    validity_verify_code = int(string1[27])
-    personal_code = string1[28:43]
-    personal_verify_code = int(string1[43])
-
-    if algorithm(passport) != passport_verify_code:
-        return "passport info error"
-    elif algorithm(birth) != birth_verify_code:
-        return "birth date info error"
-    elif algorithm(validity) != validity_verify_code:
-        return "validity info error"
-    elif algorithm(personal_code) != personal_verify_code:
-        return "personal code error"
-    else:
-        return "passed"
 
 def char_to_value(char):
-    """Returns numeric value for input char used in check digit algorithm"""
-    # Returns numeric value for input char used in check digit algorithm
+    """Returns numeric value for input char used in check digit algorithm."""
     char = char.lower()
     return CHAR_DICT[char]
 
 
-def get_check_digit(input):
-    """Takes string of alphanumeric characters and returns check digit"""
-    # Takes string of alphanumeric characters and returns check digit
+def get_check_digit(input_str):
+    """Takes string of alphanumeric characters and returns check digit."""
     weights = [7, 3, 1]
     sum_ = sum(char_to_value(char) * weights[idx % 3]
-               for idx, char in enumerate(input))
+               for idx, char in enumerate(input_str))
     return str(sum_ % 10)
 
+
 def scan_passport():
-    """Function to scan passport"""
-    #empty method
+    """Scans a passport."""
     return 'scanned'
 
+
 def extract_line1(line):
-    """Function to extract line 1"""
     # Extracts data from the first line of the passport string
     val = line.split('<')
     res = [i for i in val if i.strip()]
@@ -119,44 +80,111 @@ def extract_line1(line):
         "given_name": given_name
     }
 
-def encode(string0: str) -> str:
-    """Function to encode"""
-    string1 = string0[45:]
-    passport_number = string1[0:9]
-    country_code = string1[10:13]
-    birth_date = string1[13:19]
-    sex = string1[20]
-    expiration_date = string1[21:27]
-    personal_number = string1[28:37]
 
-    d= {}
-    k={}
-    d['line2'] =  k
-    k['passport_number'] = passport_number
-    k['country_code'] = country_code
-    k['birth_date'] = birth_date
-    k['sex'] = sex
-    k['expiration_date'] = expiration_date
-    k['personal_number'] = personal_number
+def extract_line2(line):
+    # Extracts data from the second line of the passport string
+    val = line.split('<')
+    res = [i for i in val if i.strip()]
+    passport_number = res[0][:9]
+    passport_number_check_digit = res[0][9]
+    country_code = res[0][10:13]
+    birth_date = res[0][13:19]
+    birth_date_check_digit = res[0][19]
+    sex = res[0][20]
+    expiry_date = res[0][21:27]
+    expiry_date_check_digit = res[0][27]
+    personal_number = res[0][28:]
+    personal_number_check = res[1]
+    if (
+            get_check_digit(passport_number) != passport_number_check_digit or
+            get_check_digit(birth_date) != birth_date_check_digit or
+            get_check_digit(expiry_date) != expiry_date_check_digit or
+            get_check_digit(personal_number) != personal_number_check
+    ):
+        raise ValueError("Passport Error")
+    return {
+        "passport_number": passport_number,
+        "country_code": country_code,
+        "birth_date": birth_date,
+        "sex": sex,
+        "expiry_date": expiry_date,
+        "personal_number": personal_number
+    }
 
-    return d
 
-def decode(dict):
-    """Functtion to decode"""
-    dict1 = dict["line1"]
-    dict2 = dict["line2"]
-    issc = dict1["issuing_country"]
-    lastname = dict1["last_name"]
-    givenname = dict1["given_name"]
-    passport = dict2["passport_number"]
-    country = dict2["country_code"]
-    birth = dict2["birth_date"]
-    sex = dict2["sex"]
-    exd = dict2["expiration_date"]
-    pn = dict2["personal_number"]
-    line1decode = "P<" + issc + lastname + "<<" + givenname.replace(' ','<')
-    line1decoded1 = line1decode.ljust(44,'<')
-    line2decode = passport + str(algorithm(passport)) + country + birth + str(algorithm(birth))
-    line2decode = line2decode + sex +exd + str(algorithm(exd)) + pn + "<<<<<<" + str(algorithm(pn))
-    decoded = line1decoded1 +";"+line2decode
-    return decoded
+def decode(string):
+    """
+    Takes in a string and parses it to extract MRZ data.
+    Returns data in the same format as 'records_decoded' if the string is valid,
+    prints an error and returns False if there is an issue.
+    """
+    lines = string.split(";")
+    line1 = extract_line1(lines[0])
+    line2 = extract_line2(lines[1])
+    if line1["issuing_country"] != line2['country_code']:
+        # Invalid MRZ data
+        print("Error: invalid MRZ data")
+        return False
+    result = {"line1": line1, "line2": line2}
+    result = json.dumps(result)
+    # print("records_decoded: " + result)
+    return result
+
+
+def encode(data):
+    """
+    Takes in a JSON object and generates a string for the MRZ.
+    """
+    line1 = encode_line1(data.get('line1', {}))
+    line2 = encode_line2(data.get('line2', {}))
+    result = line1 + ";" + line2
+    # print("records_encoded: " + result)
+    return result
+
+
+def encode_line1(data):
+    # accepts dict of line1 data and generates a string
+    issuing_country = data.get('issuing_country')
+    last_name = data.get('last_name')
+    given_name = data.get('given_name')
+    if not (issuing_country and last_name and given_name):
+        return 1
+    given_name = given_name.replace(" ", "<")
+    line = f'{issuing_country}{last_name}<<{given_name}'
+    line_length = 44
+    line += '<' * (line_length - len(line))
+    line += get_check_digit(issuing_country + last_name + given_name)
+    return line
+
+
+def encode_line2(data):
+    """
+    Accepts a dictionary of line2 data and generates a MRZ string.
+
+    :param data: A dictionary containing the following keys:
+                 - passport_number: A string representing the passport number.
+                 - country_code: A string representing the country code.
+                 - birth_date: A string representing the birth date in the format YYMMDD.
+                 - expiration_date: A string representing the expiration date in the format YYMMDD.
+                 - sex: A string representing the sex of the passport holder.
+                 - personal_number: A string representing the personal_number.
+                  :return: A string representing the MRZ line.
+    """
+    passport_number = data.get('passport_number')
+    passport_number_check = get_check_digit(passport_number)
+    country_code = data.get('country_code')
+    birth_date = data.get('birth_date')
+    birth_date_check = get_check_digit(birth_date)
+    expiration_date = data.get('expiration_date')
+    expiration_date_check = get_check_digit(expiration_date)
+    sex = data.get('sex')
+    personal_number = data.get('personal_number')
+    personal_number_check = get_check_digit(personal_number)
+    if not (passport_number and passport_number_check and country_code and birth_date and birth_date_check and
+            sex and expiration_date and expiration_date_check and personal_number and personal_number_check):
+        return 1
+    line = (f'{passport_number}{passport_number_check}{country_code}{birth_date}{birth_date_check}{sex}'
+            f'{expiration_date}{expiration_date_check}{personal_number}')
+    line += '<' * (43 - len(line))
+    line += personal_number_check
+    return line
